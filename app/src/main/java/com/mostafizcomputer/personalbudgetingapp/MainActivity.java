6b151190a9bd50335b32808d7f90136589592f48
Imtiaz Mohammad Tapu <imtiaz13tapu@gmail.com>
2022-01-23 21:05:02 +0600

    android apps

---
+++ app/src/main/java/com/mostafizcomputer/personalbudgetingapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.mostafizcomputer.personalbudgetingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private CardView budgetCardView , todayCardView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        budgetCardView=findViewById(R.id.budgetCardView);
+        todayCardView=findViewById(R.id.todayCardView);
+
+        budgetCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,BudgetActivity.class);
+                startActivity(intent);
+            }
+        });
+        todayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,TodaySpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
